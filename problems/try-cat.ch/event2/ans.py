e9254693f4999dfb11c34e41d8a6321747da0160
shyeon.kim <shyeon.kim@scipi.net>
2013-04-23 16:50:09 +0900

    try-catch event2
    Signed-off-by: shyeon.kim <shyeon.kim@scipi.net>

---
+++ problems/try-cat.ch/event2/ans.py
@@ -0,0 +1,40 @@
+def nextIndex(data,i,w):
+	return filter(
+			lambda x:
+			x+i>=0 and 
+			x+i<w*w and 
+			not (i%w==0 and x==-1) and
+			not (i%w==w-1 and x==1) and
+			data[x+i]==data[i]+1,
+			[-3,-1,1,3]
+			)
+
+def _snakegame(data,i,w):
+	return map(
+			lambda n:
+			_snakegame(data,n+i,w)+[data[n+i]],
+			nextIndex(data,i,w)
+		)
+
+def snakegame(data,i,w):
+	return map(
+			lambda n:
+			_snakegame(data,n,w)+[data[n]],
+			range(i,w*w)
+		)
+
+def flatten(l):
+	    return reduce(lambda x,y: x+[y] if type(y) != list else x+flatten(y), l,[])
+
+inp = []
+while 1:
+	x = raw_input().split(' ')
+	if ("" == x[0]):
+		lofl = max(snakegame(inp,0,w))
+		ret = flatten(lofl)
+		ret.reverse()
+		print ' '.join(map(str, ret))
+		break
+	w = len(x)
+	for t in x:
+		inp.append(int(t))
