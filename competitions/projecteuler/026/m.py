aebd952c3da815036b58457628eec1879cebee4a
Kim Seonghyun <shyeon.kim@scipi.net>
2014-09-18 12:24:22 +0200

    Add more refs
    Signed-off-by: Kim Seonghyun <shyeon.kim@scipi.net>

--- competitions/projecteuler/026/m.py
+++ competitions/projecteuler/026/m.py
@@ -1,5 +1,6 @@
 # http://mathworld.wolfram.com/MultiplicativeOrder.html
 # http://eli.thegreenplace.net/2009/02/25/project-euler-problem-26/
+# http://zacharydenton.com/project-euler-solutions/26/
 def _ReciprocalCycle(denom):
     # solve 10^s % d == 10^(s+t) % d
     # where t is length and s is start
