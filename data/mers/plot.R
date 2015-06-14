9de01e582da0838de63b1a1f34fb81c368e856f6
Kim Seonghyun <shyeon.kim@scipi.net>
2015-06-14 09:48:16 +0200

    Added more plots
    Signed-off-by: Kim Seonghyun <shyeon.kim@scipi.net>

--- data/mers/plot.R
+++ data/mers/plot.R
@@ -1,6 +1,6 @@
 start_index <- 1
 
-d <- c(2,3,5,7,13,15,18,24,25,30,36,42,50,64,87,95,108,122)
+d <- c(2,3,5,7,13,15,18,24,25,30,36,42,50,64,87,95,108,122,126,138,145)
 d <- d[start_index:length(d)]
 patients_log <- log(d)
 
@@ -10,8 +10,12 @@ df <- data.frame(cbind(patients_log, x))
 names(df) <- c("y","x")
 fit <- lm("y ~ x", df)
 
-jpeg("./mers_patients.jpg")
+jpeg("./mers_patients_log.jpg")
 plot(patients_log)
 abline(fit)
 title(paste0("slope = ", toString(round(fit$coefficients[2],2))))
+dev.off()
+
+jpeg("./mers_patients_log_diff.jpg")
+ts.plot(diff(patients_log))
 dev.off()
